fc554152c171e5c6fa9e70fd8a7f26799beff594
LOrena Artieda <artiedalorena@gmail.com>
2023-07-11 14:33:10 -0300

    Se agrega la funcionalidad mosrtrar peoducto al usuario

--- src/main/java/com/arg/carritodecompras/controller/HomeController.java
+++ src/main/java/com/arg/carritodecompras/controller/HomeController.java
@@ -1,16 +1,22 @@
 package com.arg.carritodecompras.controller;
 
 import com.arg.carritodecompras.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private final Logger logg= LoggerFactory.getLogger(HomeController.class);
+
     @Autowired
     private ProductoService productoService;
     @GetMapping("")
@@ -18,5 +24,11 @@ public class HomeController {
         model.addAttribute("productos", productoService.findAll());
 
         return "usuario/home";
+    }
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer
+                                           id){
+        logg.info("ID producto enviado como parametro {}", id);
+        return "usuario/productohome";
     }
 }
