1bc09ebaeb0a043c7ee25822c6ae2c05f3f5d7ae
LOrena Artieda <artiedalorena@gmail.com>
2023-07-11 22:27:53 -0300

    Creacion del metodo para generar el numero de la orden

--- src/main/java/com/arg/carritodecompras/service/IOrdenService.java
+++ src/main/java/com/arg/carritodecompras/service/IOrdenService.java
@@ -2,6 +2,9 @@ package com.arg.carritodecompras.service;
 
 import com.arg.carritodecompras.model.Orden;
 
+import java.util.List;
+
 public interface IOrdenService {
+    List<Orden> findAll();
     Orden save(Orden orden);
 }
